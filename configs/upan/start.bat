a4dc9ddfcd67166775e20bd50248264d134a771d
ileler <ileler@qq.com>
2017-06-11 08:02:07 +0800

    modify upan/start.bat abount symlink exists.

--- configs/upan/start.bat
+++ configs/upan/start.bat
@@ -40,6 +40,16 @@ if exist %U% (
 
 mklink /j %MROOT% %CROOT%
 mklink /j %CHOME% %SHOME%
+
+if exist %RHOME% (
+	rmdir %RHOME%
+)
+if exist %UHOME%\env (
+	rmdir %UHOME%\env
+)
+if exist %UHOME%\ext (
+	rmdir %UHOME%\ext
+)
 mklink /j %RHOME% %~dp0\%RC%
 mklink /j %UHOME%\env %~dp0\env
 mklink /j %UHOME%\ext %~dp0\ext
